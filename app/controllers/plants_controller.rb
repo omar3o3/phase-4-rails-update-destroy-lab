6b3ced69842d70e32d46e98e4d10f12feef6ee1e
omar3o3 <otantawy53@gmail.com>
2022-06-28 14:56:29 -0400

    commit tree 48b2e0aee732d0554648c5760e534e83f76c8def

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :plant_not_found
 
   # GET /plants
   def index
@@ -18,9 +19,27 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def plant_not_found
+    render json: {error: 'Plant not found'}, status: :not_found
   end
 end
